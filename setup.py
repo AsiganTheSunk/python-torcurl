0c1ec46666c942cd4dcad28b71a31d97a779dd5e
Pedro Arias Ruiz <asiganthesunk@gmail.com>
2017-08-31 23:41:03 +0200

    sorting things out

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name='torcurl',
     version='0.1',
     packages=['torcurl'],
-    url='https://github.com/AsiganTheSunk/torcurl2/',
+    url='https://github.com/AsiganTheSunk/torcurl/',
     license='',
     author='asigan',
     author_email='asiganthesunk@gmail.com',
-    description='python project to unite tor and pycurl'
+    description='python project to unite tor and pycurl',
+    download_url = 'hhttps://github.com/AsiganTheSunk/torcurl/archive/0.1.tar.gz',
+    keywords = ['testing', 'logging', 'example'],
+    classifiers = []
 )
